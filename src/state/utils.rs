14a8f74f16e117c54ffce6b8628f137c3bd0913e
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2026-07-06 01:28:19 +0900

    Hide year if it's in current year

--- src/state/utils.rs
+++ src/state/utils.rs
@@ -1,16 +1,33 @@
-use chrono::NaiveDate;
+use chrono::{Datelike, Local, NaiveDate};
 use icalendar::{CalendarDateTime, DatePerhapsTime};
 
 pub fn format_date_perhaps_time(dpt: &DatePerhapsTime) -> String {
-    match dpt {
-        DatePerhapsTime::Date(d) => d.format("%Y/%m/%d").to_string(),
-        DatePerhapsTime::DateTime(cdt) => match cdt {
-            CalendarDateTime::Floating(ndt) => ndt.format("%Y/%m/%d %H:%M").to_string(),
-            CalendarDateTime::Utc(utc) => utc.format("%Y/%m/%d %H:%M UTC").to_string(),
-            CalendarDateTime::WithTimezone { date_time, .. } => {
-                date_time.format("%Y/%m/%d %H:%M").to_string()
-            }
-        },
+    let year = match dpt {
+        DatePerhapsTime::Date(d) => d.year(),
+        DatePerhapsTime::DateTime(cdt) => get_naive_date(cdt).year(),
+    };
+    if year == Local::now().year() {
+        match dpt {
+            DatePerhapsTime::Date(d) => d.format("%m/%d").to_string(),
+            DatePerhapsTime::DateTime(cdt) => match cdt {
+                CalendarDateTime::Floating(ndt) => ndt.format("%m/%d %H:%M").to_string(),
+                CalendarDateTime::Utc(utc) => utc.format("%m/%d %H:%M UTC").to_string(),
+                CalendarDateTime::WithTimezone { date_time, .. } => {
+                    date_time.format("%m/%d %H:%M").to_string()
+                }
+            },
+        }
+    } else {
+        match dpt {
+            DatePerhapsTime::Date(d) => d.format("%Y/%m/%d").to_string(),
+            DatePerhapsTime::DateTime(cdt) => match cdt {
+                CalendarDateTime::Floating(ndt) => ndt.format("%Y/%m/%d %H:%M").to_string(),
+                CalendarDateTime::Utc(utc) => utc.format("%Y/%m/%d %H:%M UTC").to_string(),
+                CalendarDateTime::WithTimezone { date_time, .. } => {
+                    date_time.format("%Y/%m/%d %H:%M").to_string()
+                }
+            },
+        }
     }
 }
 
